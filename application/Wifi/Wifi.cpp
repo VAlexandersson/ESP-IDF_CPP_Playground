e66df01d09d3fd7789c7425713db13748b764389
Viktor Alexandersson <viktor.alexand@gmail.com>
2024-01-29 11:56:10 +0100

    _init for setting up wifi config. Fix limitations with hardcoded ssid and password.

--- application/Wifi/Wifi.cpp
+++ application/Wifi/Wifi.cpp
@@ -3,12 +3,72 @@
 
 namespace WIFI {
 
-char Wifi::mac_address_cstr[]{};
+char                Wifi::mac_address_cstr[]{};
+std::mutex          Wifi::init_mutex{};
+Wifi::state_e       Wifi::_state{Wifi::state_e::NOT_INITIALIZED};
+wifi_init_config_t  Wifi::wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+wifi_config_t       Wifi::wifi_config{};
 
-std::mutex Wifi::first_call_mutex;
-bool Wifi::first_call{false};
+Wifi::Wifi(void) {
 
-const esp_err_t Wifi::_get_mac(void) {
+    std::lock_guard<std::mutex> guard(init_mutex);
+
+    if(!get_mac()[0]) { 
+        if(ESP_OK != _get_mac()) 
+            esp_restart(); 
+    }
+    
+} // constructor Wifi
+
+esp_err_t Wifi::_init(void) {
+
+    std::lock_guard<std::mutex> guard(init_mutex);
+
+    esp_err_t status{ESP_OK};
+
+    if (state_e::NOT_INITIALIZED == _state) {
+        status = esp_netif_init(); // Initialize the underlying TCP/IP stack 
+
+        if (ESP_OK == status) {
+            const esp_netif_t* const p_netif = esp_netif_create_default_wifi_sta(); // Create a default WiFi station with default config
+
+            if (!p_netif) status = ESP_FAIL;
+        }
+        if (ESP_OK == status) {
+            status = esp_wifi_init(&wifi_init_config);
+        }
+        if (ESP_OK == status) {
+            status = esp_wifi_set_mode(WIFI_MODE_STA); // keep track of mode?
+        }
+        if (ESP_OK == status) {
+            
+            const size_t ssid_len_to_copy = 
+                std::min( strlen(ssid), static_cast<size_t>(sizeof(wifi_config.sta.ssid)) );
+
+            memcpy(wifi_config.sta.ssid, ssid, ssid_len_to_copy);
+
+            const size_t password_len_to_copy = 
+                std::min( strlen(password), static_cast<size_t>(sizeof(wifi_config.sta.password)) );
+
+            memcpy(wifi_config.sta.password, password, password_len_to_copy);
+
+            wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+            wifi_config.sta.pmf_cfg.capable = true;
+            wifi_config.sta.pmf_cfg.required = false;
+
+            status = esp_wifi_set_config(WIFI_IF_STA, &wifi_config); // keep track of mode?
+        }
+
+
+    } else if (state_e::ERROR == _state) {
+        status = ESP_FAIL;
+    }
+
+
+    return status;
+} // Wifi::init
+
+esp_err_t Wifi::_get_mac(void) {
 
     uint8_t mac_byte_buffer[6]{};
 
--- application/Wifi/Wifi.h
+++ application/Wifi/Wifi.h
@@ -1,14 +1,24 @@
-#pragma
+#pragma once
 
 #include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
+
 #include "esp_mac.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+
+#include <algorithm>
 #include <mutex>
+
+#include <cstring>
 
 namespace WIFI {
 
 class Wifi {
+    constexpr static const char* ssid{"WifiSSID"};
+    constexpr static const char* password{"WifiPassword"};
 public:
     enum class state_e {
         NOT_INITIALIZED,
@@ -22,30 +32,36 @@ public:
         ERROR
     };
 
-    Wifi(void) {
-
-        std::lock_guard<std::mutex> guard(first_call_mutex);
-
-        if(!first_call) {
-            if(ESP_OK != _get_mac()) esp_restart();  
-            first_call = true;
-        }
-    } // constructor Wifi
+    Wifi(void);
+    ~Wifi(void)                     = default;
+    Wifi(const Wifi&)               = default;
+    Wifi(Wifi&&)                    = default;
+    Wifi& operator=(const Wifi&)    = default;
+    Wifi& operator=(Wifi&&)         = default;
 
     esp_err_t init(void); // Set up wifi
     esp_err_t begin(void); // Start WiFi, connect WiFi... etc.
 
     state_e get_state(void);
-    const char* get_mac(void) { return mac_address_cstr; }
+
+    constexpr static const char* get_mac(void) { return mac_address_cstr; }
 
 private:
+// FUNCTIONS
+
+    static esp_err_t _init(void);
+    static wifi_init_config_t wifi_init_config;
+    static wifi_config_t wifi_config;
+
     void state_machine(void);
 
-    static char mac_address_cstr[13];
-    const esp_err_t _get_mac(void);
+    static esp_err_t _get_mac(void);
 
-    static std::mutex first_call_mutex;
-    static bool first_call;
+// VARIABLES
+    static state_e      _state;
+    static char         mac_address_cstr[13];
+    static std::mutex   init_mutex;
+
 }; // class Wifi
 
 
